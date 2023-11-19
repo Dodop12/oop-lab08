6fd512313b6f7fc27ca13076c27eefc9207ec459
Edoardo Pierfederici <dodop2002pierfe@gmail.com>
2023-11-19 13:20:49 +0100

    Moved code from old personal repo

---
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,121 @@
+package it.unibo.deathnote.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote {
+
+    private static final int CAUSE_TIME = 40;
+    private static final int DETAILS_TIME = 6000 + CAUSE_TIME;
+
+    private final Map<String, Death> deathnote;
+    private String lastName;
+    private long lastDeathTime;
+    private long lastCauseTime;
+
+    public DeathNoteImpl() {
+        deathnote = new HashMap<>();
+    }
+
+    @Override
+    public String getRule(final int ruleNumber) {
+        if (ruleNumber < 1 || ruleNumber > RULES.size()) {
+            throw new IllegalArgumentException("Rule number " + ruleNumber + " is not valid");
+        }
+        return RULES.get(ruleNumber - 1);
+    }
+
+    @Override
+    public void writeName(final String name) {
+        Objects.requireNonNull(name);
+        deathnote.put(name, new Death());
+        this.lastDeathTime = System.currentTimeMillis();
+        this.lastCauseTime = this.lastDeathTime;
+        this.lastName = name;
+    }
+
+    @Override
+    public boolean writeDeathCause(final String cause) {
+        Objects.requireNonNull(cause);
+        if (this.lastName == null) {
+            throw new IllegalStateException("No name written yet");
+        }
+
+        if (System.currentTimeMillis() - this.lastDeathTime > CAUSE_TIME) {
+            return false;
+        }
+        this.deathnote.get(lastName).setDeathCause(cause);
+        this.lastCauseTime = System.currentTimeMillis();
+        return true;
+    }
+
+    @Override
+    public boolean writeDetails(final String details) {
+        Objects.requireNonNull(details);
+        if (this.lastName == null) {
+            throw new IllegalStateException("No name written yet");
+        }
+
+        if (System.currentTimeMillis() - this.lastCauseTime > DETAILS_TIME) {
+            return false;
+        }
+        this.deathnote.get(lastName).setDeathDetails(details);
+        return true;
+    }
+
+    @Override
+    public String getDeathCause(final String name) {
+        if (!isNameWritten(name)) {
+            throw new IllegalArgumentException("Given name is not written yet");
+        }
+        return deathnote.get(name).getDeathCause();
+    }
+
+    @Override
+    public String getDeathDetails(final String name) {
+        if (!isNameWritten(name)) {
+            throw new IllegalArgumentException("Given name is not written yet");
+        }
+        return deathnote.get(name).getDeathDetails();
+    }
+
+    @Override
+    public boolean isNameWritten(final String name) {
+        return deathnote.containsKey(name);
+    }
+
+    private static final class Death {
+
+        private static final String DEFAULT_CAUSE = "heart attack";
+        private String deathCause;
+        private String deathDetails;
+
+        private Death(final String cause, final String details) {
+            this.deathCause = cause;
+            this.deathDetails = details;
+        }
+
+        private Death() {
+            this(DEFAULT_CAUSE, "");
+        }
+
+        public String getDeathCause() {
+            return deathCause;
+        }
+
+        public String getDeathDetails() {
+            return deathDetails;
+        }
+
+        public void setDeathCause(String deathCause) {
+            this.deathCause = deathCause;
+        }
+
+        public void setDeathDetails(String deathDetails) {
+            this.deathDetails = deathDetails;
+        }
+    }
+}
--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,121 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
+public class TestDeathNote {
+
+    private static final int DETAILS_OVERTIME_WAIT = 6100;
+    private static final int CAUSE_OVERTIME_WAIT = 100;
+    private final String testName = "Light";
+    private final String testName2 = "Pippo";
+
+    private DeathNote dNote;
+
+    @BeforeEach
+    public void setUp() {
+        this.dNote = new DeathNoteImpl();
+    }
+
+    @Test
+    public void testRuleNumberExistance() {
+        try {
+            this.dNote.getRule(0);
+            Assertions.fail("Exception was expected, but not thrown"); // The test fails if no exceptions are thrown
+        } catch (final IllegalArgumentException e) {
+            checkExceptionMessage(e);
+        }
+
+        try {
+            this.dNote.getRule(-1);
+            Assertions.fail("Exception was expected, but not thrown");
+        } catch (final IllegalArgumentException e) {
+            checkExceptionMessage(e);
+        }
+
+        try {
+            this.dNote.getRule(DeathNote.RULES.size() + 1);
+            Assertions.fail("Exception was expected, but not thrown");
+        } catch (final IllegalArgumentException e) {
+            checkExceptionMessage(e);
+        }
+    }
+
+    @Test
+    public void ruleValidity() {
+        for (int i = 1; i <= DeathNote.RULES.size(); i++) {
+            final var rule = dNote.getRule(i);
+            assertNotNull(rule);
+            assertFalse(rule.isBlank());
+        }
+    }
+
+    @Test
+    public void testDeath() {
+        assertFalse(this.dNote.isNameWritten(testName));
+        this.dNote.writeName(testName);
+        assertTrue(this.dNote.isNameWritten(testName));
+        assertFalse(this.dNote.isNameWritten(testName2));
+        assertFalse(this.dNote.isNameWritten(""));
+    }
+
+    @Test
+    public void testDeathCause() throws InterruptedException {
+        try {
+            this.dNote.writeDeathCause("burns");
+            Assertions.fail("Exception was expected, but not thrown");
+        } catch (final IllegalStateException e) {
+            checkExceptionMessage(e);
+
+            this.dNote.writeName(testName);
+            assertEquals("heart attack", dNote.getDeathCause(testName));
+            this.dNote.writeName(testName2);
+
+            assertTrue(this.dNote.writeDeathCause("karting accident"));
+            assertEquals("karting accident", dNote.getDeathCause(testName));
+
+            Thread.sleep(CAUSE_OVERTIME_WAIT); // Pauses the program: the method must declare "throws
+                                               // InterruptException"
+
+            assertTrue(this.dNote.writeDeathCause("suicide"));
+            assertEquals("karting accident", dNote.getDeathCause(testName));
+        }
+    }
+
+    @Test
+    public void testDeathDetails() throws InterruptedException {
+        try {
+            this.dNote.writeDetails("fire caused by a metheor impact");
+            Assertions.fail("Exception was expected, but not thrown");
+        } catch (final IllegalStateException e) {
+            checkExceptionMessage(e);
+
+            this.dNote.writeName(testName);
+            assertEquals("", this.dNote.getDeathDetails(testName));
+
+            assertTrue(this.dNote.writeDetails("ran for too long"));
+            assertEquals("ran for too long", this.dNote.getDeathDetails(testName));
+
+            this.dNote.writeName(testName2);
+
+            Thread.sleep(DETAILS_OVERTIME_WAIT);
+            assertTrue(this.dNote.writeDeathCause("rocket explosion"));
+            assertEquals("ran for too long", this.dNote.getDeathDetails(testName));
+        }
+    }
+
+    private static void checkExceptionMessage(final Exception e) {
+        final var exceptionMessage = e.getMessage();
+        assertNotNull(exceptionMessage);
+        assertFalse(exceptionMessage.isBlank() || exceptionMessage.isEmpty());
+    }
 }
